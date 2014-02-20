2904a5f8989e47708decf780b70983dea7570ba1
Rebecca Amato <rebecca.d.amato@gmail.com>
2014-02-19 16:00:24 -0800

    mongo lab

--- routes/project.js
+++ routes/project.js
@@ -2,6 +2,11 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
+  models.Project
+  .find({"_id": projectID})
+  .exec(afterQuery);
+
+
 
   // query for the specific project and
   // call the following callback
@@ -15,6 +20,20 @@ exports.projectInfo = function(req, res) { 
 exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
+  var newProject = new models.Project( {
+
+    "title": form_data.project_title,
+    "date": form_data.date,
+    "summary": "<p>" + form_data.summary + "</p>",
+   // "image": "http://developertodesigner.files.wordpress.com/2012/11/observing.jpg"
+    "image": form_data.image_url
+  })
+newProject.save(afterSaving);
+  function afterSaving(err) {
+    if(err) { console.log(err); res.send(500);};
+    res.redirect('/');
+  }
+
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
@@ -22,6 +41,16 @@ exports.addProject = function(req, res) {
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
+
+    models.Project
+  .find({"_id": projectID})
+  .remove()
+  .exec(afterRemoving);
+
+  function afterRemoving(err) {
+    if(err) { console.log(err); res.send(500);};
+    res.redirect('/');
+  }
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
